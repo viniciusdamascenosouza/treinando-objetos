169e5f4cdf651555216487717de5490cdc3ccb44
viniciusdamascenosouza <viniciusdamascenosouza@gmail.com>
2022-12-20 07:52:09 -0300

    segundo

--- acesso_ponto.js
+++ acesso_ponto.js
@@ -3,14 +3,15 @@ const cliente = {
     idade: 32,
     cpf: "82938372982",
     email: "joao@dominio.com",
-    tipo: "cliente VIP"
+    tipo: "cliente VIP",
+    teste: "teste"
 }
 
 console.log(`${cliente["nome"]} tem ${cliente["idade"]} anos de idade, seu email é ${cliente["email"]}. ${cliente.nome} é um ${cliente["tipo"]}.`);
 
 console.log(`Os três primeiros digitos do cpf são: ${cliente["cpf"].substring(0,3)}`)
 
-const chaves = ["nome", "idade", "cpf", "email", "tipo"];
+const chaves = ["nome", "idade", "cpf", "email", "tipo", "teste"];
 
 chaves.forEach( (chave)=> {
     console.log(`A chave "${chave}" tem valor "${cliente[chave]}"`)
